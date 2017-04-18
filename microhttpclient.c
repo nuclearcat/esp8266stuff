fa265aaa811c36f0ac11600640db78451d79e372
Denys Fedoryshchenko <denys@visp.net.lb>
2017-04-18 07:11:40 +0300

    even more minimalistic

--- microhttpclient.c
+++ microhttpclient.c
@@ -6,14 +6,14 @@
  *
  * Extremely minimalistic handling of tcp inbound data to parse basic HTTP/1.0
  * Just after request sent, do something like this:
+ * .. Establishing TCP, sending GET /url HTTP/1.0\r\nHeaders\r\n\r\n ...
  * do {
  *  ret = recv(s, recv_buf, UPG_BUF_SZ, MSG_DONTWAIT);
- *  if (ret > 0) {
- *       state = parse_http(state, recv_buf, &ret, &fwupcb);
- *  }
+ *  if (ret > 0)
+ *       parse_http(&state, recv_buf, &ret, &fwupcb);
  * }
  * Where fwupcb will be callback that will receive "body" data
- * WARNING! I dont do supplied params check, so callback must be not NULL
+ * WARNING! I dont do supplied params check, so callback must be not NULL, size > 0, etc
  * Still experimental.
  */
 #include "esp_common.h"
@@ -25,39 +25,38 @@
 #define BODY 3
 #define ERROR 255
 
-uint8_t parse_http(uint8_t state, char *buf, int *size, void *callback) {
+void parse_http(uint8_t *state, char *buf, int *size, void *callback) {
         int i = 0;
         void (*ptrFunc)(char*,int);
         ptrFunc=callback;
 
         /* We are on state of reading body, directly do callback */
-        if (state == BODY) {
+        if (*state == BODY) {
                 ptrFunc(buf, *size);
-                return(state);
+                return;
         }
 
         for (i=0; i<*size; i++) {
                 switch(buf[i]) {
                 case '\n':
-                        state = NEWLINE;
+                        *state = NEWLINE;
                         break;
                 case '\r':
-                        if (state == NEWLINE) {
+                        if (*state == NEWLINE) {
                                 // Still we might have some body left in chunk
                                 int offset = i+2;
-                                state = BODY;
+                                *state = BODY;
                                 if (offset < *size) {
                                         ptrFunc(&buf[offset], *size-offset);
-                                        *size -= (offset);
+                                        *size -= offset;
                                 }
-                                i=(*size); //to terminate loop
+                                return;
                         }
                         break;
                 default:
-                        if (state == NEWLINE)
-                                state = READING;
+                        if (*state == NEWLINE)
+                                *state = READING;
                         break;
                 }
         }
-        return(state);
 }
